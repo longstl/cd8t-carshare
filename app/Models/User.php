361bfc754d66a06ce39ffc55467c7a81b28ef2da
Dac Phuong <phuonghdth2009010@gmail.com>
2021-04-24 17:42:57 +0700

    crud user template admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,14 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -36,6 +38,7 @@ class User extends Authenticatable
         'is_pet_allowed',
         'music_preference',
         'chitchat_preference',
+        'role'
     ];
     public function userCars(){
         return $this->hasMany(UserCar::class);
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,9 +16,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/test', function () {
     return view('Admin/user/form');
+});
+Route::prefix('admin/user')->group(function (){
+    Route::get('',[UserController::class,'list'])->name('listUser');
+    Route::get('create',[UserController::class,'create'])->name('createUser');
+    Route::post('create',[UserController::class,'store'])->name('storeUser');
+    Route::get('update/{id}',[UserController::class,'update'])->name('updateUser');
+    Route::post('update/{id}',[UserController::class,'save'])->name('saveUser');
+    Route::get('delete/{id}',[UserController::class,'delete'])->name('deleteUser');
 });
 
