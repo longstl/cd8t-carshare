1c6942605c8309ce05119bfdfb0be5f2c64c40ad
Quan Do <quanddth2009043@fpt.edu.vn>
2021-04-25 19:10:39 +0700

    Added Match api

--- app/Http/Controllers/Admin/AdminRideController.php
+++ app/Http/Controllers/Admin/AdminRideController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Enums\RequestStatus;
+use App\Enums\RideStatus;
 use App\Http\Controllers\Controller;
 use App\Models\Feedback;
 use App\Models\Request;
 use App\Models\Ride;
+use Carbon\Carbon;
+use DateTime;
 
 
 class AdminRideController extends Controller
@@ -45,21 +49,39 @@ class AdminRideController extends Controller
     public function findMatch($id)
     {
         $ride = Ride::find($id);
-        $requests = Request::all();
+        $requests = Request::query()
+            ->where('status', RequestStatus::WAITING)
+            ->where('desired_pickup_time', '<', Carbon::now())
+            ->where('seats_occupy', '<=', $ride['seats_available'])
+            ->orderBy('desired_pickup_time')
+            ->get();
         $matched_requests = [];
         foreach ($requests as $request) {
             $request['destination_difference'] = getDistance($request['destination_address'], $ride['destination_address'])['distance']['value'];
             if ($request['destination_difference'] > 1000) {
                 continue;
             }
-            $request['pickup_time'] = getDistance($ride['origin_address'], $request['pickup_address'])['duration']['value'];
+            $request['duration'] = getDistance($ride['origin_address'], $request['pickup_address'])['duration']['value'];
             array_push($matched_requests, $request);
         }
         return $matched_requests;
     }
 
-    public function match($id)
+    public function match($ride_id, $request_id, $duration)
     {
-        
+        $ride = Ride::find($ride_id);
+        $request = Request::find($request_id);
+        $request->status = RequestStatus::MATCHED;
+        $request->ride_id = $ride_id;
+        try {
+            $time = new DateTime($ride['travel_start_time']);
+            $time->modify('+'.$duration.' minutes');
+            $request->pickup_time = $time;
+        } catch (\Exception $e) {
+        }
+        $request->save();
+        $ride->status = RideStatus::MATCHED;
+        $ride->save();
+        return $ride;
     }
 }
--- database/migrations/2021_04_24_030335_create_requests_table.php
+++ database/migrations/2021_04_24_030335_create_requests_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enums\RideRequestStatus;
+use App\Enums\RequestStatus;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -23,7 +23,7 @@ class CreateRequestsTable extends Migration
             $table->string('destination_coordinate')->nullable();
             $table->dateTime('desired_pickup_time');
             $table->integer('seats_occupy');
-            $table->integer('status')->default(RideRequestStatus::WAITING);
+            $table->integer('status')->default(RequestStatus::WAITING);
             $table->integer('ride_id')->nullable();
             $table->dateTime('pickup_time')->nullable();
             $table->float('price')->nullable();
