bf2301163efeb5a5d813726e816c17eaf7685242
Your Name <thangpdth2009031@fpt.edu.vn>
2021-04-26 09:57:46 +0700

    add ride_detail, request_detail

--- resources/views/web/request_detail.blade.php
+++ resources/views/web/request_detail.blade.php
@@ -5,67 +5,67 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        .table > tbody > tr > td {
+            padding: 13px!important;
+        }
+    </style>
 @include('web.layout.Style')
     <title>Request Detail | Car Share</title>
 </head>
 <body>
 @include('web.layout.header')
-<div class="root">
-    <div class="container_content">
-        <div class="content">
-            <h1 class="text-dark">Request detail</h1><br>
-            <div class="col-12 col-md-12">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Names of fields</th>
-                        <th scope="col">Values</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Origin</td>
-                        <td>182 Hoa Bằng, Yên Hoà, Cầu Giấy, Hà Nội, Vietnam</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Destination</td>
-                        <td>458 Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội, Vietnam</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Start time</td>
-                        <td>12/12/2021 , 15h30p</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Estimated distance</td>
-                        <td>3.5 km</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Estimated travel time</td>
-                        <td>12 mins</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Amount of people</td>
-                        <td>7 people</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="btn_container">
+<div id="wrapper" class="clearfix">
+    <section id="content">
 
+        <div class="content-wrap">
+
+            <div class="container clearfix">
+
+                <!-- Post Content
+                ============================================= -->
+                <div class="postcontent nobottommargin clearfix" style="margin: auto!important; float: none;">
+                    <h4>Request detail</h4>
+                    <table class="table" style="margin-bottom: 0 !important;">
+                        <tbody>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Origin</h5></td>
+                            <td>182 Hoa Bằng, Yên Hoà, Cầu Giấy, Hà Nội, Vietnam</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Destination</h5></td>
+                            <td>458 Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội, Vietnam</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Start time</h5></td>
+                            <td>12/12/2021 , 15h30p</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Estimated distance</h5></td>
+                            <td>3.5 km</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Estimated travel time</h5></td>
+                            <td>12 mins</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Amount of people</h5></td>
+                            <td>7 people</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Price</h5></td>
+                            <td>$300</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <div class="line" style="margin: 0 0 20px 0;"></div>
                     <button class="btn btn-primary">Cancel</button>
-                </div>
+                </div><!-- .postcontent end -->
             </div>
         </div>
-    </div>
+    </section>
     @include('web.layout.footer')
 </div>
-
 @include('web.layout.script')
 
 </body>
--- resources/views/web/ride_details.blade.php
+++ resources/views/web/ride_details.blade.php
@@ -5,70 +5,68 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"><meta http-equiv="X-UA-Compatible" content="ie=edge">
     @include('web.layout.Style')
+    <style>
+        .table > tbody > tr > td {
+            padding: 13px!important;
+        }
+    </style>
     <title>Ride Details | Car Share</title>
 </head>
 <body>
 @include('web.layout.header')
-<div >
-    <div class="container_content">
-        <div class="content">
-            <h1 class="text-dark">Rider detail</h1><br>
-            <div class="col-12 col-md-12">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Names of fields</th>
-                        <th scope="col">Values</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Origin</td>
-                        <td>182 Hoa Bằng, Yên Hoà, Cầu Giấy, Hà Nội, Vietnam</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Destination</td>
-                        <td>458 Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội, Vietnam</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Start time</td>
-                        <td>12/12/2021 , 15h30p</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Estimated distance</td>
-                        <td>3.5 km</td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Number of available seats</td>
-                        <td>3 seats</td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Estimated travel time</td>
-                        <td>12 mins</td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>Model rider</td>
-                        <td>mẫu xe gì đó</td>
-                    </tr>
-                    </tbody>
-                </table>
-                <div class="btn_container">
+<div id="wrapper" class="clearfix">
+    <section id="content">
+        <div class="content-wrap">
+            <div class="container clearfix">
+                <!-- Post Content
+                ============================================= -->
+                <div class="postcontent nobottommargin clearfix" style="margin: auto!important; float: none;">
+                    <h4>Ride detail</h4>
+                    <table class="table" style="margin-bottom: 0 !important;">
+                        <thead>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Origin</h5></td>
+                            <td>182 Hoa Bằng, Yên Hoà, Cầu Giấy, Hà Nội, Vietnam</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Destination</h5></td>
+                            <td>458 Minh Khai, Vĩnh Phú, Hai Bà Trưng, Hà Nội, Vietnam</td>
+                        </tr>
+                        <tr>
+                            <td><h5 style="margin: 0!important;">Start time</h5></td>
+                            <td>12/12/2021 , 15h30p</td>
+                        </tr>
+                        <h5>
+                            <td><h5 style="margin: 0!important;">Estimated distance</h5></td>
+                            <td>3.5 km</td>
+                            </tr>
+                            <tr>
+                                <td><h5 style="margin: 0!important;">Number of available seats</h5></td>
+                                <td>7 people</td>
+                            </tr>
+                            <tr>
+                                <td><h5 style="margin: 0!important;">Estimated travel time</h5></td>
+                                <td>12 mins</td>
+                            </tr>
+                            <tr>
+                                <td><h5 style="margin: 0!important;">Smoking allowed in the car</h5></td>
+                                <td>No</td>
+                            </tr>
+                            <td><h5 style="margin: 0!important;">Music preferences</h5></td>
+                            <td>Yes</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <div class="line" style="margin: 0 0 20px 0;"></div>
                     <button class="btn btn-primary">Cancel</button>
-                </div>
+                </div><!-- .postcontent end -->
             </div>
         </div>
-    </div>
+    </section>
     @include('web.layout.footer')
 </div>
-
 @include('web.layout.script')
 </body>
 </html>
