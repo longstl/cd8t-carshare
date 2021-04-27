d8b1a1587021b49d7aadfa127af04390ca7f81f7
Your Name <thangpdth2009031@fpt.edu.vn>
2021-04-27 15:32:05 +0700

    error

--- resources/views/web/layout/header.blade.php
+++ resources/views/web/layout/header.blade.php
@@ -14,7 +14,7 @@
                 <a href="/" class="retina-logo" data-dark-logo="{{lib_assets('web/images/logo.png')}}"><img
                         src="{{lib_assets('web/images/logo.png')}}" alt="Canvas Logo"></a>
             </div>
-            <i style="font-size: 35px;margin: 10px;float: right;" class="fa fa-bars menu-bars-icon iconbar" aria-hidden="true"></i>
+            <i style="font-size: 25px;float: right;" class="fa fa-bars menu-bars-icon iconbar" aria-hidden="true"></i>
             <nav id="primary-menu" class="menu_top">
                 <ul class="h-100">
                     <li class="current"><a href="{{route('index')}}">
@@ -59,7 +59,7 @@
                     @endif
                     <div style="width: 20%;float: left">
                         <a>
-                            <i style="font-size: 20px" class="fa fa-bell" aria-hidden="true">
+                            <i style="font-size: 20px;cursor: pointer" class="fa fa-bell" aria-hidden="true">
                                 <p style=" display: flex;justify-content: center;align-items: center;height: 14px;width: 14px;font-size: 10px;border-radius: 50%;background: #08eff8
                                     ;position: absolute;top: -8px;right: -12px; cursor: pointer">3</p></i>
                         </a>
@@ -87,10 +87,10 @@
                 @if(\Illuminate\Support\Facades\Auth::check())
                     <div id="account" class="accountn" style="border-top: 2px solid #1ABC9C;box-shadow: 0 2px 4px rgb(0 0 0 / 20%), 0 -1px 0px rgb(0 0 0 / 2%);width: 130px;color: black; background-color: #ffffff;position: absolute;  top: 100%; right: 87px;">
                         <div style="padding: 5px;border-bottom: 1px solid rgba(0,0,0,0.1)" class="log-hover">
-                            <a href="{{route('profile_user')}}" style="display: inline-block;text-align: center;-ms-touch-action: manipulation;border: 1px solid transparent;padding: 6px 12px;line-height: 1.42857143;width: 100%;;color: black;margin-right: 5px"><b>Profile</b></a>
+                            <a href="{{route('profile_user')}}" style="display: inline-block;text-align: center;-ms-touch-action: manipulation;border: 1px solid transparent;padding: 6px 12px;line-height: 1.42857143;width: 100%;;color: black;margin-right: 5px"><b>Profile<i style="padding-left: 5px" class="fas fa-id-badge"></i></b></a>
                         </div>
                         <div style="padding: 5px" class="log-hover">
-                            <a href="{{route('logoutUser')}}" style="display: inline-block;text-align: center;-ms-touch-action: manipulation;border: 1px solid transparent;padding: 6px 12px;line-height: 1.42857143;width: 100%;color: black"><b>Logout</b><i class="fas fa-sign-out-alt" style="padding-left: 5px"></i></a>
+                            <a href="{{route('logoutUser')}}" style="display: inline-block;text-align: center;-ms-touch-action: manipulation;border: 1px solid transparent;padding: 6px 12px;line-height: 1.42857143;width: 100%;color: black"><b>Logout<i class="fas fa-sign-out-alt" style="padding-left: 5px"></i></b></a>
                         </div>
                     </div>
                 @endif
